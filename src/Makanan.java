1d36bfeb6dded183e63cf512dc9b4ab80182d4fd
Kevin Basuki <kevinb9g.17@gmail.com>
2018-04-24 01:06:35 +0700

    Checked with Checkstyle (Google Checks)

--- src/Makanan.java
+++ src/Makanan.java
@@ -1,27 +1,30 @@
-public class Makanan extends BendaMati{
-    private boolean edible;
+public class Makanan extends BendaMati {
+  private boolean edible;
 
-    public Makanan(){
+  public Makanan(){
 
-    }
-    public Makanan(double X, double Y){
-        super(X,Y);
-        edible = true;
-    }
+  }
 
-    public boolean isEdible(){
-        return edible;
-    }
-    public void setEdible(boolean b){
-        edible = b;
-    }
+  public Makanan(double x, double y) {
+    super(x,y);
+    edible = true;
+  }
 
-    //Implementasi Operator==
-    public boolean isSama(Makanan m){
-        return (getX() == m.getX() && getY() == m.getY());
-    }
-    //Implementasi Operator!=
-    public boolean isBeda(Makanan m){
-        return (getX() != m.getX() || getY() != m.getY());
-    }
+  public boolean isEdible() {
+    return edible;
+  }
+
+  public void setEdible(boolean b) {
+    edible = b;
+  }
+
+  //Implementasi Operator==
+  public boolean isSama(Makanan m) {
+    return (getX() == m.getX() && getY() == m.getY());
+  }
+
+  //Implementasi Operator!=
+  public boolean isBeda(Makanan m) {
+    return (getX() != m.getX() || getY() != m.getY());
+  }
 }
