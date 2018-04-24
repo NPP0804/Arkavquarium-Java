4ce2fedadb718df3bbb80a74a176f6acf63d3874
Shinta Ayu C Kemala <shinta990405@gmail.com>
2018-04-25 01:28:10 +0700

    Element and ElementTest implemented.

--- src/Element.java
+++ src/Element.java
@@ -1,24 +1,46 @@
 public class Element<T> {
     protected Element<T> next;
     protected T info;
-
+    
+    /**
+     * Class Constructor.
+     * @param next Pointer to next element.
+     * @param info value of Element.
+     */
     public Element(Element<T> next, T info) {
         this.next = next;
         this.info = info;
     }
-
+    
+    /**
+     * Next Getter.
+     * @return next Element.
+     */
     public Element<T> getNext() {
         return next;
     }
-
+    
+    /**
+     * Set Next with another element.
+     * @param next Element which position after this element.
+     */
     public void setNext(Element<T> next) {
         this.next = next;
     }
-
+    
+    /**
+     * Info Getter.
+     * @return info value of Element.
+     */
     public T getInfo() {
         return info;
     }
-
+    
+    /**
+     * Info Setter, set info with new value.
+     * @param info new value of Element.
+     */
+    @SuppressWarnings("unused")
     public void setInfo(T info) {
         this.info = info;
     }
--- src/ElementTest.java
+++ src/ElementTest.java
@@ -1,22 +1,40 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ElementTest {
-
-    @Test
-    void getNext() {
-    }
-
-    @Test
-    void setNext() {
-    }
-
-    @Test
-    void getInfo() {
-    }
-
-    @Test
-    void setInfo() {
-    }
+  private Element<Integer> e1;
+  private Element<Integer> e2;
+  
+  @BeforeEach
+  void setUp() {
+    e1 = new Element<>(null, 0);
+    e2 = new Element<>(null, 8);
+  }
+  
+  @Test
+  void getNext() {
+    assertNull(e1.getNext());
+    e1.setNext(e2);
+    assertSame(e1.getNext(),e2);
+  }
+  
+  @Test
+  void setNext() {
+    e2.setNext(e1);
+    assertSame(e2.next, e1);
+  }
+  
+  @Test
+  void getInfo() {
+    assertEquals(0, (int) e1.getInfo());
+    assertEquals(8, (int) e2.getInfo());
+  }
+  
+  @Test
+  void setInfo() {
+    e1.setInfo(-78);
+    assertEquals(-78, (int) e1.info);
+  }
 }
