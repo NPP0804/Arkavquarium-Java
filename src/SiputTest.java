7e6045e3f06ca2b4ba601f62085ed5f9fdbfdb8b
Kevin Basuki <kevinb9g.17@gmail.com>
2018-04-25 12:30:38 +0700

    JUnit succeed

--- src/SiputTest.java
+++ src/SiputTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SiputTest {
-  private Siput s1;
+  private Siput s1 = new Siput();
 
   @Test
   public void moveTowardsTarget() {
